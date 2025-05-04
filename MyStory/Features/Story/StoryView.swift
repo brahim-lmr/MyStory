7960ef383deb5e01af20cd66a454f4f79e6542b3
Brahim Lamri <brahim.llmr@gmail.com>
2025-05-04 16:29:44 +0200

    Update the StoryView's UI

--- MyStory/Features/Story/StoryView.swift
+++ MyStory/Features/Story/StoryView.swift
@@ -15,8 +15,29 @@ struct StoryView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
-                Text(user.username)
+            ZStack {
+                AsyncImage(url: URL(string: "https://picsum.photos/300/500?random=\(UUID().uuidString)")!){ image in
+                    image
+                        .resizable()
+                    
+                } placeholder: {
+                    ProgressView()
+                }
+                
+                
+                VStack {
+                    
+                    createProgressView()
+                    
+                    Spacer()
+                    HStack {
+                        createReactionButton(type: .like)
+                        createReactionButton(type: .dislike)
+                        
+                    }
+                    .padding(.bottom, 16)
+                }
+                
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -26,6 +47,38 @@ struct StoryView: View {
                 }
             }
         }
+    }
+    
+    private func createReactionButton(type: Reaction) -> some View {
+        Button(action: {}) {
+            Image(systemName: type == .dislike ? "hand.thumbsdown" : "hand.thumbsup")
+                .resizable()
+                .frame(width: 28, height: 28)
+                .foregroundColor(.white)
+                .padding()
+                .background(.ultraThinMaterial)
+                .clipShape(Circle())
+            
+        }
+    }
+    
+    private func createProgressView() -> some View {
+        HStack(spacing: 4) {
+            ForEach(0...3, id: \.self) { index in
+                Capsule()
+                    .fill(Color.white)
+                    .frame(height: 4)
+                    .frame(maxWidth: .infinity)
+                
+            }
+            
+        }
+        .padding(.top, 16)
+        .padding(.horizontal, 12)
+    }
+    
+    enum Reaction {
+        case like, dislike
     }
 }
 
