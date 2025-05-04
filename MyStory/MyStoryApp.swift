c8556bf8b1206dbd3bc34d9a2540e0ede4f150d7
Brahim Lamri <brahim.llmr@gmail.com>
2025-05-04 13:53:01 +0200

    Initial Commit

---
+++ MyStory/MyStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoryApp.swift
+//  MyStory
+//
+//  Created by Brahim Lamri on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyStoryTests/MyStoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoryTests.swift
+//  MyStoryTests
+//
+//  Created by Brahim Lamri on 04/05/2025.
+//
+
+import Testing
+@testable import MyStory
+
+struct MyStoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
